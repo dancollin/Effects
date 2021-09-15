4c8eff1e9d7ef0a1548c2032eee33fb583bc6d1e
Dan Collin <dan.collin@learnet.se>
2021-09-15 12:44:24 +0200

    First commit

---
+++ code.js
@@ -0,0 +1,47 @@
+
+let playground = document.querySelector("#playground");
+
+playground.width = 500;
+playground.height = 500;
+
+let play = playground.getContext('2d');
+let i = 0;
+let arcs = [];
+const AMAX = 3000;
+const RADMAX = 10;
+let TRANSP = 0.1;
+let mouseX=0, mouseY=0;
+
+for( let i =0; i<AMAX; i++) {
+    let a = {};
+    a.x = Math.round(Math.random()*(playground.width-50)+25);
+    a.y = Math.round(Math.random()*(playground.height-50)+25);
+    a.rad = Math.round(Math.random()*RADMAX+15);
+    a.pos = Math.round(Math.random()*Math.PI);
+    a.speed = Math.round(Math.random()*400)/10000;
+    arcs.push(a);
+}
+
+
+setInterval(funny, 25);
+
+function funny() {
+    play.fillStyle="white";
+    play.fillRect(0,0,playground.width, playground.height);
+
+
+
+    //play.fill();
+    play.fillStyle = "rgba(0,0,0,0.1";
+    play.strokeStyle = `rgba(0,0,0,${TRANSP})`;
+    for(let i = 0; i<arcs.length; i++) {
+        let t = arcs[i];
+        play.beginPath();
+        play.arc(t.x, t.y, t.rad, t.pos, t.pos+3.14);
+        arcs[i].pos += arcs[i].speed;
+        play.closePath();
+        play.stroke();
+    } 
+}
+
+ 
